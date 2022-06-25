5c7355a745cea9d10e3cfa8fde2a68e34b74b0ce
cbserra <789723+cbserra@users.noreply.github.com>
2022-06-24 22:52:11 -0700

    adding express

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
 
 server.use(middlewares);
 server.use(router);
+
+server.use('/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 server.listen(port);
